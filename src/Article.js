e0e9b8cf80d2b658bde081a912182183182b264b
Marie Otaki <otakimarie@gmail.com>
2019-10-09 08:18:52 +0700

    modified a width of image from contentful

--- src/Article.js
+++ src/Article.js
@@ -1,6 +1,7 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactMarkdown from "react-markdown";
 import Contentful from "./Contentful";
+import "./Article.css";
 
 class Article extends React.Component {
   state = {
@@ -9,11 +10,10 @@ class Article extends React.Component {
 
   async componentDidMount() {
     const { slug } = this.props.match.params;
-    // Contentful api 叩いて記事データ取得
+    //get article from contentful API
     const contentful = new Contentful();
     try {
       const article = await contentful.getArtcle(undefined, "blogPost", slug);
-      console.log(article);
       this.setState({ articleFromContentful: article.items });
     } catch (err) {
       console.log("errorです");
@@ -22,15 +22,27 @@ class Article extends React.Component {
   }
 
   render() {
-    console.log(this.state.articleFromContentful);
-
     const bodyOfArticle = this.state.articleFromContentful.map(data => {
       return data.fields.body;
     });
 
     console.log(bodyOfArticle);
 
-    return <div>{bodyOfArticle}</div>;
+    return (
+      <div className="article-container">
+        <div className="article-content">{bodyOfArticle}</div>
+        <img
+          src="//images.ctfassets.net/79vndjll5vfu/6As4qiAJzaAKy3P25X8MUW/424cb0cadb2ae6624e02c0b009c02bca/cosco_hotdog_2.jpeg"
+          alt="article-img"
+          className="article-img"
+          width="300px"
+        ></img>
+        {/* <ReactMarkdown
+          source="![Espen](https://espen.codes/assets/img/avatar-colored.jpg)"
+          renderers={{ image: Image }}
+        /> */}
+      </div>
+    );
   }
 }
 
