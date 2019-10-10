0cceaedb5c7a61e537564a6810ed22e1937f3c80
Marie Otaki <otakimarie@gmail.com>
2019-10-10 18:13:24 +0700

    modified header

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,58 @@
 import React from "react";
 import logo from "./img/vanStudentsInfo_logo.png";
+import Contentful from "./Contentful";
+import { Route, Link } from "react-router-dom";
 import "./Header.css";
 
 class Header extends React.Component {
+  state = {
+    arrayData: []
+  };
+
+  getDataFromContentful = async () => {
+    const contentful = new Contentful();
+    const response = await contentful.getArtcles();
+    this.setState({
+      arrayData: response.items
+    });
+  };
+
+  componentDidMount() {
+    this.getDataFromContentful();
+  }
   render() {
+    const tagArray = this.state.arrayData.map(data => {
+      return data.fields.tags;
+    });
+
+    let tagData = [];
+    tagArray.forEach(data => {
+      data.forEach(child => {
+        tagData.push(child);
+      });
+    });
+
+    const tagLists = tagData.map(data => {
+      switch (data) {
+        case "ランチ":
+          return <li>Food</li>;
+        case "コスパ":
+          return <li>Good Value</li>;
+        case "スーパー":
+          return <li>Shopping</li>;
+        default:
+          return <li>default</li>;
+      }
+    });
+
     return (
       <div id="header">
-        <img src={logo} alt="logo" id="logo"></img>
-        <form id="form1" action="https://vanstudents.com" method="get">
-          <input
-            id="sbox1"
-            name="sbox"
-            type="text"
-            placeholder="キーワードを入力"
-          />
-          <input id="sbtn1" type="submit" value="検索" />
-        </form>
+        <div className="header-logo">
+          <Link to={"/"}>
+            <img src={logo} alt="logo" id="logo"></img>
+          </Link>
+        </div>
+        <ul className="header-nav">{tagLists}</ul>
       </div>
     );
   }
